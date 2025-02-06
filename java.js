558874fb82cb0f5a5c0e41bbeb9ba7315ad6a3db
dianaloza <lozaval95@gmail.com>
2025-02-06 20:38:42 +0100

    Añadido java.js, style.css y modificado index.html

---
+++ java.js
@@ -0,0 +1,27 @@
+
+/*Crear modo input de la contraseña que ingreses si es <8 caacteres te diga que es muy corta si es enre 8 y 10 es de seguridad intermedia y si es > 10 es segura
+*/
+
+const passwordInput = document.getElementById("password");
+const messageElement =  document.getElementById("message");
+
+btn.addEventListener("input", function() {
+    const contraseña = passwordInput.value;
+    let message = "";
+    let messageClass = "";
+
+    if (contraseña.length < 8) {
+        message = "La contraseña es muy corta";
+        messageClass ="corta";
+
+    } else if (contraseña.length >= 8 && contraseña.length <= 10) {
+        message = "La contraseña es de seguridad intermedia";
+        messageClass ="intermedia";
+    } else {
+        message = "La contraseña es segura";
+        messageClass = "segura";
+    }
+
+    messageElement.textContent = message;
+    messageElement.className = messageClass;
+});
